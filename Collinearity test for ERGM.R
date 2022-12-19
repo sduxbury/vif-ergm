46af07b35a7615bf32609ec30c36b5cdad5052c6
Scott Duxbury <34458357+sduxbury@users.noreply.github.com>
2022-12-19 14:17:03 -0500

    Update Collinearity test for ERGM.R
    Include functionality for mlergm and btergm

--- Collinearity test for ERGM.R
+++ Collinearity test for ERGM.R
@@ -1,78 +1,64 @@
-##Measure to detect collinearity in ERGM
-#Based on Duxbury (2018) in Sociological Methods and Research
+#' Function to assess multicollinearity in ERGM using variance inflation factors
+#' Measure based on Duxbury (2018) available in Sociological Methods and Research
+#'
+#' @param my.ergm is an ergm object
+#'
 
 
-###====================
-#=========ERGM========
-#====================
+
+vif.ergm<-function(my.ergm){
 
 
-###my.ergm is an ERGM object inherited from ergm package--available via statnet
-VIF.ERGM<-function(my.ergm){
-  require(ergm)
+  if(class(my.ergm)%in%"btergm"){
+    data_mat<-my.ergm@effects
+    corr5<-stats::cor(data_mat[!rownames(data_mat)%in%"edges",
+                        !colnames(data_mat)%in%"edges"]) ##omit edges term
+    beta<-btergm::coef(my.ergm)
+  }else{
 
-  cor.mat<-cov2cor(my.ergm$covar) #calculate correlation matrix
-  corr5<-cor.mat[-c(1),-c(1)] ##omit edges term
-  
+      #get correlation matrix
+    if(class(my.ergm)%in%"mlergm"){
+      cor.mat<-stats::cov2cor(solve(my.ergm$information_matrix))
+      beta<-my.ergm$theta
+
+    }else{
+      cor.mat<-stats::cov2cor(my.ergm$covar) #calculate correlation matrix
+      beta<-stats::coef(my.ergm)
+
+    }
+
+    #omit edges, assign names to matrix
+    rownames(cor.mat)<-colnames(cor.mat)<-names(beta)
+    corr5<-cor.mat[!rownames(cor.mat)%in%"edges",
+                   !colnames(cor.mat)%in%"edges"]
+  }
+
   corr5<-corr5[!is.na(corr5[1:nrow(corr5)]),]
   corr5<-corr5[,which(!is.na(corr5[1,1:ncol(corr5)]))]
-  
-  VIFS<-matrix(0,nr=1,nc=ncol(corr5))
-  
+
+  VIFS<-matrix(0,nrow=1,ncol=ncol(corr5))
+
   for(i in 1:ncol(corr5)){
-    
+
     gvec<-as.vector(corr5[-c(i),i]) ##create vector of correlations between covariate of interest and other covariates in the model
-    tgvec<-t(gvec)            
+    tgvec<-t(gvec)
     xcor<-solve(corr5[-c(i),-c(i)]) ##create square matrix of correlations between covariates in the model other than the one of interest
     Rsq<-tgvec%*%xcor%*%gvec
     VIFS[1,i]<-1/(1-Rsq)
   }
-  ##Columns are covariates as they appear in the SAOM object
-  colnames(VIFS)<-names(my.ergm$coef[-c(1)])
- message("Higher values indicate greater correlation.\nVIF > 20 is concerning, VIF > 100 indicates severe collinearity.")
+
+  colnames(VIFS)<-names(beta[!names(beta)%in%"edges"])
+
+  if(class(my.ergm)%in%"btergm"){
+
+    warning("VIFS for bootstrap TERGM based on model matrix, not the covariance matrix of the estimator. Benchmarks used for MCMC ML estimation may not apply.")
+
+   }else{
+    message("Higher values indicate greater correlation.\nVIF > 20 is concerning, VIF > 100 indicates severe collinearity.")
+
+  }
   VIFS
 }
 
 
-VIF.ERGM(my.ergm)
-
-
-
-
-
-
-
-
-
-#================================
-###=========Example==============
-#=================================
-
-##Not run
-
-
-##simulate network
-library(igraph)
-A<-erdos.renyi.game(75,.1,type="gnp")
-
-##convert to sna object
-a<-get.adjacency(A)
-a<-as.matrix(a)
-
-library(statnet)
-a<-as.network(a,directed=FALSE)
-
-
-##run ERGM
-m1<- ergm( a ~ edges + kstar(2)+meandeg+
-             gwesp(decay=.7, fixed=TRUE),
-           control=control.ergm(seed=40))
-
-##evaluate collinearity
-VIF.ERGM(m1)
-
-
-
-           
-  
 
